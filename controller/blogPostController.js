5cbab4c87743a6435475816688613e5b90dd4805
Lucas G Silveira <lucgs88@gmail.com>
2022-03-30 15:18:53 -0300

    fix: lint

--- controller/blogPostController.js
+++ controller/blogPostController.js
@@ -5,7 +5,6 @@ const { getToken } = require('../utils/token');
 
 const { getUserIdByEmail } = require('./userController');
 
-// eslint-disable-next-line max-lines-per-function
 const getBlogPosts = async (_req, res) => {
   try {
     const blogPosts = await BlogPost.findAll({
@@ -13,13 +12,11 @@ const getBlogPosts = async (_req, res) => {
         model: User,
         as: 'user',
         attributes: { exclude: ['password'] },
-      },
-      {
+      }, {
         model: Category,
         as: 'categories',
         through: { attributes: [] },
-      },
-    ], 
+      }],
       raw: true,
   });
     return res.status(200).json(blogPosts);
@@ -29,14 +26,10 @@ const getBlogPosts = async (_req, res) => {
   }
 };
 
-// eslint-disable-next-line max-lines-per-function
 const postBlogPost = async (req, res) => {
   try {
     const token = getToken(req.headers);
-    console.log('getToken: ', token);
     const user = await getUserIdByEmail(token);
-    console.log('getUserIdByEmail: ', user);
-
     const { title, content, categoryIds } = req.body;
     const post = await BlogPost.create({
       title,
